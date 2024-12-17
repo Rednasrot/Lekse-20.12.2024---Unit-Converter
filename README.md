96ad0b97d8bd0b700dd5ead6c5b3113199fd51c1
Rednasrot <Rednasrot@gmail.com>
2024-12-17 14:58:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Lekse-20.12.2024---Unit-Converter
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Rednasrot/Lekse-20.12.2024---Unit-Converter)
---
+++ script.js
@@ -0,0 +1,17 @@
+const input = document.querySelector('#input');
+const output = document.querySelector('#convertedunit');
+const unit = document.querySelector('#unitconverter');
+const button = document.querySelector('#submit-button');
+
+button.addEventListener('click', function (event) {
+  event.preventDefault();
+  if (unit.value === 'meters') {
+    output.textContent = input.value * 1;
+  } else if (unit.value === 'centi-meters') {
+    output.textContent = input.value * 100;
+  } else if (unit.value === 'inches') {
+    output.textContent = input.value * 39.3700787;
+  } else if (unit.value === 'feet') {
+    output.textContent = input.value * 3.2808399;
+  }
+});
