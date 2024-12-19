ee47f1a1c0eef78a3c3adfcb2ace5ddd0396a403
Rednasrot <rednasrot@gmail.com>
2024-12-19 11:53:57 +0100

    oppdatering v0.2 Addet flere elementer+output text

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ const output = document.querySelector('#convertedunit');
 const unit = document.querySelector('#unitconverter');
 const button = document.querySelector('#submit-button');
 
-button.addEventListener('click', function (event) {
+button.addEventListener('click', function (event){
   event.preventDefault();
   if (unit.value === 'meters') {
-    output.textContent = input.value * 1;
+    output.textContent = input.value * 1 + " Meters";
   } else if (unit.value === 'centi-meters') {
-    output.textContent = input.value * 100;
+    output.textContent = input.value * 100 + " Centi-meters";
   } else if (unit.value === 'inches') {
-    output.textContent = input.value * 39.3700787;
+    output.textContent = input.value * 39.3700787 + " Inches";
   } else if (unit.value === 'feet') {
-    output.textContent = input.value * 3.2808399;
+    output.textContent = input.value * 3.2808399 + " Feet";
   } else if (unit.value === 'footballfield') {
-    output.textContent = input.value * 0.010936132983377079;
+    output.textContent = input.value * 0.010936132983377079 + " Football fields";
 }
 });
